03639ca1291c157156632f367b23e4dad4d66fec
berdjabrahamian <berdj.abrahamian@gmail.com>
2020-03-09 17:42:53 -0400

    Init Logs Model

---
+++ database/factories/LogsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Auction\Logs;
+use Faker\Generator as Faker;
+
+$factory->define(Logs::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_03_09_213452_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('auction_id');
+            $table->integer('store_id');
+            $table->string('activity');
+            $table->integer('customer_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
