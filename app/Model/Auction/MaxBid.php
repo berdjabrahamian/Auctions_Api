e50c18294f861b1fece65b9e51a76c0d4859d87c
berdjabrahamian <berdj.abrahamian@gmail.com>
2020-03-29 16:24:17 -0400

    Max Bid Model updates

--- app/Model/Auction/MaxBid.php
+++ app/Model/Auction/MaxBid.php
@@ -11,10 +11,26 @@ class MaxBid extends Model
     protected $table      = 'max_bids';
     public    $timestamps = true;
     protected $fillable   = ['store_id', 'auction_id', 'customer_id', 'amount', 'outbid'];
-
+    protected $hidden     = [
+        'store_id',
+    ];
+    protected $appends    = ['amount_cents'];
+    protected $casts      = [
+        'amount_cents' => 'int',
+        'outbid'       => 'bool',
+    ];
 
     protected $dispatchesEvents = [
         'updating' => UpdatingMaxBidEvent::class,
     ];
+
+    /**
+     * We transform the amount attribute, which is a dollar value into cents and give it its own attribute;
+     * @return int
+     */
+    public function getAmountCentsAttributes(): int
+    {
+        return $this->amount * 100;
+    }
 
 }
